5857ee55eec0f708c8f7efbac65c1d7f157bb565
Edvard Vindenes Steenslid <142335301+h676669@users.noreply.github.com>
2025-10-24 21:25:25 +0200

    fant den beste modelen

---
+++ website.py
@@ -0,0 +1,7 @@
+# for å kunne bruke dette lokalt, kjør: streamlit run website.py
+# for å installere alt riktig aktiver py inviroment: .venv\Scripts\Activate.ps1
+# så installer streamlit: pip install streamlit
+
+
+import streamlit as st
+st.write("Hello, world!")
